44e8d4f1b7a2d323f863c8afa90033d6b57b15ee
Jay-540 <96350406+Jay-540@users.noreply.github.com>
2022-12-29 13:39:43 +0300

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,9 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +9,7 @@
  * Description: singly linked list node structure
  * for project
  */
-
-typedef strunt listint_s
+typedef struct listint_s
 {
 int n;
 struct listint_s *next;
@@ -21,5 +18,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-
 #endif /* LISTS_H */
